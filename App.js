853b5bef35cdf739a7697ee84364b64e889f5c29
[jiraLee] <[jrlee@ocube.co.kr]>
2019-05-10 13:49:38 +0900

    codePush code

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to OCUBE!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
         <TouchableOpacity onPress ={this.onButtonPress}>
